4333953f5e0f64abb72001e883ebf1775b60ebe5
pikoneeeee <100016474+pikoneeeee@users.noreply.github.com>
2022-11-08 11:38:58 +0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,17 @@
+from website import create_app
+
+app = create_app()
+
+@app.after_request
+def add_header(r):
+    """
+    Add headers to both force latest IE rendering engine or Chrome Frame,
+    and also to cache the rendered page for 10 minutes.
+    """
+    r.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    r.headers["Pragma"] = "no-cache"
+    r.headers["Expires"] = "0"
+    return r
+
+if __name__ == "__main__":
+    app.run(debug=True)
